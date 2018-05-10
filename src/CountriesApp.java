5e0a2be8f498f7e2c88f7ea382834a16b3e4b012
jillxx <jiaoxu84@gmail.com>
2018-05-09 23:41:43 -0400

    extended complete

--- src/CountriesApp.java
+++ src/CountriesApp.java
@@ -5,25 +5,27 @@ public class CountriesApp {
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to the Countries Maintenance Application!");
-		CountriesTextFile.createFile();
+		// CountriesTextFile.createFile();
 		Scanner scnr = new Scanner(System.in);
 		// show the menu:
 		int menuNum = 0;
-		while (menuNum != 3) {
-			System.out.println("\n1 - See the list of countries\n2 - Add a country\n3 - Exit");
+		while (menuNum != 4) {
+			System.out.println("\n1 - See the list of countries\n2 - Add a country\n3 - Delete a country\n4 - Exit");
 
-			menuNum = Validator.getInt(scnr, "\nEnter menu number: ", 1, 3);
+			menuNum = Validator.getInt(scnr, "\nEnter menu number: ", 1, 4);
 
 			if (menuNum == 1) {
-				// menu1.
+				// menu1.display country list
 				System.out.println();
-				CountriesTextFile.readFromFile();
-
+				CountriesBinaryFile.readFromFile();
+				// menu2 add a country
 			} else if (menuNum == 2) {
 				String country = Validator.getString(scnr, "Enter country: ");
-				CountriesTextFile.writeToFile(country);
-			} else {
-
+				CountriesBinaryFile.writeToFile(country);
+				// menu3 delete country
+			} else if (menuNum == 3) {
+				String deleteCountry = Validator.getString(scnr, "What country do you want to delete from the list: ");
+				CountriesBinaryFile.removeLineFromFile(deleteCountry);
 			}
 
 		}
--- src/CountriesBinaryFile.java
+++ src/CountriesBinaryFile.java
@@ -1,12 +1,117 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class CountriesBinaryFile {
-public static void main(String[] args) {
-	
-	
-	
-	
-	
-	
-}
-}
+	public static void main(String[] args) {
+		createFile();
+		writeToFile("USA\nIndia\nCanada\nFrance");
 
+	}
+
+	public static void readFromFile() {
+		Path readFile = Paths.get("countries.dat");
+		File file = readFile.toFile();// convert to a file object.
+
+		try {
+			FileReader fr = new FileReader(file);// read char to char
+
+			BufferedReader reader = new BufferedReader(fr);// read blocks of info
+
+			String line = reader.readLine();
+
+			while (line != null) {
+				System.out.println(line);
+				line = reader.readLine();
+			}
+
+			reader.close();// close the file reader too
+
+		} catch (IOException e) {
+			System.out.println("Something went wrong");
+		}
+
+	}
+
+	public static void writeToFile(String country) {
+
+		Path writeFile = Paths.get("countries.dat");
+		File file = writeFile.toFile();
+
+		try { // true add info to the file, false rewrite the file
+			PrintWriter outW = new PrintWriter(new FileOutputStream(file, true)); // FileOutput Stream can take 2
+																					// parameters
+			outW.println(country);
+			outW.close();// flushes data closes the stream
+			System.out.println("This country has been saved!");
+
+		} catch (FileNotFoundException e) {
+			System.out.println("The file was not found here...");
+		}
+
+	}
+
+	public static void createFile() {
+
+		Path filePath = Paths.get("countries.dat");
+
+		if (Files.notExists(filePath)) {
+			try {
+				Files.createFile(filePath);
+				System.out.println("Your file was created successfully");
+			} catch (IOException e) {
+				System.out.println("Something went wrong with the file creation");
+			}
+		}
+
+		System.out.println("File Name: " + filePath.getFileName());
+		System.out.println("Absolute Path: " + filePath.toAbsolutePath());
+	}
+
+	public static void removeLineFromFile(String lineToRemove) {
+
+		Path readFile = Paths.get("countries.dat");// get the path of the file
+		File file = readFile.toFile();// convert to a file object.
+		Path writeFile = Paths.get("temp.dat"); // create a temp file path
+		File file1 = writeFile.toFile();// create a temp object.
+
+		try {
+			FileReader fr = new FileReader(file);// read char to char
+
+			BufferedReader reader = new BufferedReader(fr);// read blocks of info
+			PrintWriter outW = new PrintWriter(new FileOutputStream(file1));
+
+			String line = null;
+
+			while ((line = reader.readLine()) != null) {
+
+				if (!line.equalsIgnoreCase(lineToRemove)) {
+
+					outW.println(line);
+				}
+			}
+
+			outW.close();
+			reader.close();// close the file reader too
+			// Delete the original file
+			if (!file.delete()) {
+				System.out.println("Could not delete file");
+				return;
+			}
+
+			// Rename the new file to the filename the original file had.
+			if (!file1.renameTo(file))
+				System.out.println("Could not rename file");
+		} catch (IOException e) {
+			System.out.println("Something went wrong");
+		}
+
+	}
+}
